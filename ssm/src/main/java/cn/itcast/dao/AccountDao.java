73b330dc7cd34891772d2165e2542e88ccdc5a98
xiaqiang <765986088@qq.com>
2018-10-16 22:25:13 +0800

    把本地工程添加到git1

---
+++ ssm/src/main/java/cn/itcast/dao/AccountDao.java
@@ -0,0 +1,24 @@
+package cn.itcast.dao;
+
+import cn.itcast.domain.Account;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface AccountDao {
+    /**
+     * 查询所有账户
+     * @return
+     */
+    @Select("select * from account")
+    public List<Account> findAll();
+
+    /**
+     * 保存账户
+     * @param account
+     */
+    @Insert("insert into account(name, money) values(#{name}, #{money})")
+    public void saveAccount(Account account);
+
+}
